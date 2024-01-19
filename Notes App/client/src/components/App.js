5318e5017b143911500e262ced1d82390b0475ab
Ihsan <127175839+ihsanakm@users.noreply.github.com>
2024-01-20 00:07:20 +0530

    created navigation bar

--- Notes App/client/src/components/App.js
+++ Notes App/client/src/components/App.js
@@ -1,54 +1,28 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Note from "./Note";
 import LogInPage from "../pages/logInPage";
 import SigninPage from "../pages/signinPage";
-import RequireAuth  from "./RequireAuth";
+import RequireAuth from "./RequireAuth";
+import NavigationBar from "./NavBar";
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavLink
-          to="/"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/login"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-        >
-          Log In
-        </NavLink>
-        <NavLink
-          to="/signup"
-          className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""
-          }
-        >
-          Sign Up
-        </NavLink>
-
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <RequireAuth>
-                <Note />
-              </RequireAuth>
-            }
-          />
-          <Route path="/login" element={<LogInPage />} />
-          <Route path="/signup" element={<SigninPage />} />
-
-        </Routes>
-      </BrowserRouter>
+      <>
+          <NavigationBar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Note />
+                </RequireAuth>
+              }
+            />
+            <Route path="/login" element={<LogInPage />} />
+            <Route path="/signup" element={<SigninPage />} />
+          </Routes>
+      </>
     </div>
   );
 }
--- Notes App/client/src/index.js
+++ Notes App/client/src/index.js
@@ -4,12 +4,15 @@ import App from "./components/App";
 import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./components/authStore";
+import { BrowserRouter } from "react-router-dom";
 
 axios.defaults.baseURL = "http://localhost:3000";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    <BrowserRouter>
     <Provider store={store}>
             <App />
     </Provider>
+    </BrowserRouter>
 )
